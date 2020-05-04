75b52f0815601f5050b4d63fa3638ad6be0a5f88
Matt Landreman <mattland@umd.edu>
2020-05-04 14:10:25 -0400

    renamed sigma to varpi in r_singularity. Replaced some exponentials with multiplication for speed.

--- quasisymmetry_max_r_before_singularity.f90
+++ quasisymmetry_max_r_before_singularity.f90
@@ -11,9 +11,9 @@ subroutine quasisymmetry_max_r_before_singularity(d_Z20_d_zeta, d_Z2s_d_zeta, d_
   real(dp) :: g0, g1c, g20, g2s, g2c, sigma_denominator, abs_costheta, denominator, r, residual, rc
   real(dp) :: K0, K2s, K2c, K4s, K4c, sintheta, costheta, sin2theta, cos2theta, lp
   real(dp) :: coefficients(5), real_parts(4), imag_parts(4)
-  real(dp) :: quadratic_A, quadratic_B, quadratic_C, radical, sigma
+  real(dp) :: quadratic_A, quadratic_B, quadratic_C, radical, varpi
   real(dp) :: start_time, end_time
-  real(dp) :: abs_cos2theta, residual_if_sigma_plus, residual_if_sigma_minus
+  real(dp) :: abs_cos2theta, residual_if_varpi_plus, residual_if_varpi_minus
   integer :: max_j_phi
   logical :: local_verbose = .false.
   !logical :: local_verbose = .true.
@@ -33,12 +33,12 @@ subroutine quasisymmetry_max_r_before_singularity(d_Z20_d_zeta, d_Z2s_d_zeta, d_
 
      !g1s = -2*X20(j)*Y1c(j) + 2*X2c(j)*Y1c(j) + 2*X2s(j)*Y1s(j) + 2*X1c(j)*Y20(j) - 2*X1c(j)*Y2c(j)
 
-     g1c = -2*X2s(j)*Y1c(j) + 2*X20(j)*Y1s(j) + 2*X2c(j)*Y1s(j) + 2*X1c(j)*Y2s(j) - X1c(j)**2*Y1s(j)*curvature(j)
+     g1c = -2*X2s(j)*Y1c(j) + 2*X20(j)*Y1s(j) + 2*X2c(j)*Y1s(j) + 2*X1c(j)*Y2s(j) - X1c(j)*X1c(j)*Y1s(j)*curvature(j)
 
      g20 = -4*lp*X2s(j)*Y2c(j) + 4*lp*X2c(j)*Y2s(j) + lp*X1c(j)*X2s(j)*Y1c(j)*curvature(j) - &
           2*lp*X1c(j)*X20(j)*Y1s(j)*curvature(j) - lp*X1c(j)*X2c(j)*Y1s(j)*curvature(j) - &
-          lp*X1c(j)**2*Y2s(j)*curvature(j) + 2*lp*Y1c(j)*Y1s(j)*Z2c(j)*torsion(j) - &
-          lp*X1c(j)**2*Z2s(j)*torsion(j) - lp*Y1c(j)**2*Z2s(j)*torsion(j) + lp*Y1s(j)**2*Z2s(j)*torsion(j) - &
+          lp*X1c(j)*X1c(j)*Y2s(j)*curvature(j) + 2*lp*Y1c(j)*Y1s(j)*Z2c(j)*torsion(j) - &
+          lp*X1c(j)*X1c(j)*Z2s(j)*torsion(j) - lp*Y1c(j)*Y1c(j)*Z2s(j)*torsion(j) + lp*Y1s(j)*Y1s(j)*Z2s(j)*torsion(j) - &
           Y1s(j)*Z20(j)*d_X1c_d_zeta(j) - Y1s(j)*Z2c(j)*d_X1c_d_zeta(j) + &
           Y1c(j)*Z2s(j)*d_X1c_d_zeta(j) - X1c(j)*Z2s(j)*d_Y1c_d_zeta(j) - &
           X1c(j)*Z20(j)*d_Y1s_d_zeta(j) + X1c(j)*Z2c(j)*d_Y1s_d_zeta(j) + &
@@ -46,9 +46,9 @@ subroutine quasisymmetry_max_r_before_singularity(d_Z20_d_zeta, d_Z2s_d_zeta, d_
 
      g2c = -4*lp*X2s(j)*Y20(j) + 4*lp*X20(j)*Y2s(j) + &
           lp*X1c(j)*X2s(j)*Y1c(j)*curvature(j) - lp*X1c(j)*X20(j)*Y1s(j)*curvature(j) - &
-          2*lp*X1c(j)*X2c(j)*Y1s(j)*curvature(j) - lp*X1c(j)**2*Y2s(j)*curvature(j) + &
-          2*lp*Y1c(j)*Y1s(j)*Z20(j)*torsion(j) - lp*X1c(j)**2*Z2s(j)*torsion(j) - &
-          lp*Y1c(j)**2*Z2s(j)*torsion(j) - lp*Y1s(j)**2*Z2s(j)*torsion(j) - &
+          2*lp*X1c(j)*X2c(j)*Y1s(j)*curvature(j) - lp*X1c(j)*X1c(j)*Y2s(j)*curvature(j) + &
+          2*lp*Y1c(j)*Y1s(j)*Z20(j)*torsion(j) - lp*X1c(j)*X1c(j)*Z2s(j)*torsion(j) - &
+          lp*Y1c(j)*Y1c(j)*Z2s(j)*torsion(j) - lp*Y1s(j)*Y1s(j)*Z2s(j)*torsion(j) - &
           Y1s(j)*Z20(j)*d_X1c_d_zeta(j) - Y1s(j)*Z2c(j)*d_X1c_d_zeta(j) + &
           Y1c(j)*Z2s(j)*d_X1c_d_zeta(j) - X1c(j)*Z2s(j)*d_Y1c_d_zeta(j) + &
           X1c(j)*Z20(j)*d_Y1s_d_zeta(j) - X1c(j)*Z2c(j)*d_Y1s_d_zeta(j) + &
@@ -56,11 +56,11 @@ subroutine quasisymmetry_max_r_before_singularity(d_Z20_d_zeta, d_Z2s_d_zeta, d_
 
      g2s = 4*lp*X2c(j)*Y20(j) - 4*lp*X20(j)*Y2c(j) + &
           lp*X1c(j)*X20(j)*Y1c(j)*curvature(j) - lp*X1c(j)*X2c(j)*Y1c(j)*curvature(j) - &
-          2*lp*X1c(j)*X2s(j)*Y1s(j)*curvature(j) - lp*X1c(j)**2*Y20(j)*curvature(j) + &
-          lp*X1c(j)**2*Y2c(j)*curvature(j) - lp*X1c(j)**2*Z20(j)*torsion(j) - &
-          lp*Y1c(j)**2*Z20(j)*torsion(j) + lp*Y1s(j)**2*Z20(j)*torsion(j) + &
-          lp*X1c(j)**2*Z2c(j)*torsion(j) + lp*Y1c(j)**2*Z2c(j)*torsion(j) + &
-          lp*Y1s(j)**2*Z2c(j)*torsion(j) + Y1c(j)*Z20(j)*d_X1c_d_zeta(j) - &
+          2*lp*X1c(j)*X2s(j)*Y1s(j)*curvature(j) - lp*X1c(j)*X1c(j)*Y20(j)*curvature(j) + &
+          lp*X1c(j)*X1c(j)*Y2c(j)*curvature(j) - lp*X1c(j)*X1c(j)*Z20(j)*torsion(j) - &
+          lp*Y1c(j)*Y1c(j)*Z20(j)*torsion(j) + lp*Y1s(j)*Y1s(j)*Z20(j)*torsion(j) + &
+          lp*X1c(j)*X1c(j)*Z2c(j)*torsion(j) + lp*Y1c(j)*Y1c(j)*Z2c(j)*torsion(j) + &
+          lp*Y1s(j)*Y1s(j)*Z2c(j)*torsion(j) + Y1c(j)*Z20(j)*d_X1c_d_zeta(j) - &
           Y1c(j)*Z2c(j)*d_X1c_d_zeta(j) - Y1s(j)*Z2s(j)*d_X1c_d_zeta(j) - &
           X1c(j)*Z20(j)*d_Y1c_d_zeta(j) + X1c(j)*Z2c(j)*d_Y1c_d_zeta(j) - &
           X1c(j)*Z2s(j)*d_Y1s_d_zeta(j) + X1c(j)*Y1s(j)*d_Z2s_d_zeta(j)
@@ -72,25 +72,25 @@ subroutine quasisymmetry_max_r_before_singularity(d_Z20_d_zeta, d_Z2s_d_zeta, d_
      ! We end up with the form in "20200322-01 Max r for GarrenBoozer.docx":
      ! K0 + K2s*sin(2*theta) + K2c*cos(2*theta) + K4s*sin(4*theta) + K4c*cos(4*theta) = 0.
 
-     K0 = 2*g1c**2*g20 - 3*g1c**2*g2c + 8*g0*g2c**2 + 8*g0*g2s**2
+     K0 = 2*g1c*g1c*g20 - 3*g1c*g1c*g2c + 8*g0*g2c*g2c + 8*g0*g2s*g2s
 
-     K2s = 2*g1c**2*g2s
+     K2s = 2*g1c*g1c*g2s
 
-     K2c = -2*g1c**2*g20 + 2*g1c**2*g2c
+     K2c = -2*g1c*g1c*g20 + 2*g1c*g1c*g2c
 
-     K4s = g1c**2*g2s - 16*g0*g2c*g2s
+     K4s = g1c*g1c*g2s - 16*g0*g2c*g2s
 
-     K4c = g1c**2*g2c - 8*g0*g2c**2 + 8*g0*g2s**2
+     K4c = g1c*g1c*g2c - 8*g0*g2c*g2c + 8*g0*g2s*g2s
 
-     coefficients(1) = 4*(K4c**2 + K4s**2)
+     coefficients(1) = 4*(K4c*K4c + K4s*K4s)
 
      coefficients(2) = 4*(K4s*K2c - K2s*K4c)
 
-     coefficients(3) = K2s**2 + K2c**2 - 4*K0*K4c - 4*K4c**2 - 4*K4s**2
+     coefficients(3) = K2s*K2s + K2c*K2c - 4*K0*K4c - 4*K4c*K4c - 4*K4s*K4s
 
      coefficients(4) = 2*K0*K2s + 2*K4c*K2s - 4*K4s*K2c
 
-     coefficients(5) = (K0 + K4c)**2 - K2c**2
+     coefficients(5) = (K0 + K4c)*(K0 + K4c) - K2c*K2c
 
      call quasisymmetry_quartic_roots(coefficients, real_parts, imag_parts)
 
@@ -122,30 +122,30 @@ subroutine quasisymmetry_max_r_before_singularity(d_Z20_d_zeta, d_Z2s_d_zeta, d_
            cycle
         end if
 
-        ! Determine sigma by checking which choice gives the smaller residual in the K equation
+        ! Determine varpi by checking which choice gives the smaller residual in the K equation
         abs_cos2theta = sqrt(1 - sin2theta * sin2theta)
-        residual_if_sigma_plus  = abs(K0 + K2s * sin2theta + K2c *   abs_cos2theta &
+        residual_if_varpi_plus  = abs(K0 + K2s * sin2theta + K2c *   abs_cos2theta &
              + K4s * 2 * sin2theta *   abs_cos2theta  + K4c * (1 - 2 * sin2theta * sin2theta))
-        residual_if_sigma_minus = abs(K0 + K2s * sin2theta + K2c * (-abs_cos2theta) &
+        residual_if_varpi_minus = abs(K0 + K2s * sin2theta + K2c * (-abs_cos2theta) &
              + K4s * 2 * sin2theta * (-abs_cos2theta) + K4c * (1 - 2 * sin2theta * sin2theta))
 
-        if (residual_if_sigma_plus > residual_if_sigma_minus) then
-           sigma = -1
+        if (residual_if_varpi_plus > residual_if_varpi_minus) then
+           varpi = -1
         else
-           sigma = 1
+           varpi = 1
         end if
-        cos2theta = sigma * abs_cos2theta
+        cos2theta = varpi * abs_cos2theta
 
-        ! The next few lines give an older method for computing sigma, which has problems in edge cases
-        ! where w (the root of the quartic polynomial) is very close to +1 or -1, giving sigma
+        ! The next few lines give an older method for computing varpi, which has problems in edge cases
+        ! where w (the root of the quartic polynomial) is very close to +1 or -1, giving varpi
         ! not very close to +1 or -1 due to bad loss of precision.
         !
-        !sigma_denominator = ((K4s*2*sin2theta + K2c) * sqrt(1 - sin2theta*sin2theta))
-        !if (abs(sigma_denominator) < 1e-8) print *,"WARNING!!! sigma_denominator=",sigma_denominator
-        !sigma = -(K0 + K2s * sin2theta + K4c*(1 - 2*sin2theta*sin2theta)) / sigma_denominator
-        !if (abs(sigma*sigma-1) > 1e-3) print *,"WARNING!!! abs(sigma*sigma-1) =",abs(sigma*sigma-1)
-        !sigma = nint(sigma) ! Ensure sigma is exactly either +1 or -1.
-        !cos2theta = sigma * sqrt(1 - sin2theta*sin2theta)
+        !varpi_denominator = ((K4s*2*sin2theta + K2c) * sqrt(1 - sin2theta*sin2theta))
+        !if (abs(varpi_denominator) < 1e-8) print *,"WARNING!!! varpi_denominator=",varpi_denominator
+        !varpi = -(K0 + K2s * sin2theta + K4c*(1 - 2*sin2theta*sin2theta)) / varpi_denominator
+        !if (abs(varpi*varpi-1) > 1e-3) print *,"WARNING!!! abs(varpi*varpi-1) =",abs(varpi*varpi-1)
+        !varpi = nint(varpi) ! Ensure varpi is exactly either +1 or -1.
+        !cos2theta = varpi * sqrt(1 - sin2theta*sin2theta)
 
         abs_costheta = sqrt(0.5*(1 + cos2theta))
         if (local_verbose) print *,"  jr=",jr,"  sin2theta=",sin2theta,"  cos2theta=",cos2theta
